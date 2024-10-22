34006cf735557cded3290a5e8eade091f75f6646
quanphatdev <lequanphat3579@gmail.com>
2024-10-22 16:35:34 +0700

    feat/setup-app-config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { AllConfigType } from './config/config.type';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  const configService = app.get(ConfigService<AllConfigType>);
+
+  app.enableShutdownHooks();
+
+  app.setGlobalPrefix(
+    configService.getOrThrow('app.apiPrefix', { infer: true }),
+    {
+      exclude: [],
+    },
+  );
 
   // config swagger
   const options = new DocumentBuilder()
@@ -16,6 +32,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(configService.getOrThrow('app.port', { infer: true }));
 }
 void bootstrap();
